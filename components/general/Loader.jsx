c26abe50a792cd841dd27c854fbed6e340da18cb
Yash Jha <jhayash99@gmail.com>
2023-05-03 05:08:33 +0530

    fixed imports

---
+++ components/general/Loader.jsx
@@ -0,0 +1,24 @@
+import { Box, CircularProgress } from "@mui/material";
+import React from "react";
+
+const Loader = ({
+    color = "secondary",
+    size = 40,
+    fullscreen = false,
+    height = "100%",
+}) => {
+    return (
+        <Box
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            height={fullscreen ? "calc(100vh - 80px)" : height}
+            width="100%"
+            zIndex={100}
+        >
+            <CircularProgress color={color} size={size} />
+        </Box>
+    );
+};
+
+export default Loader;
